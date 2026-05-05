a50a6ff472059fb24c58a5e24aad776cfc8ca8b1
Krypton867 <v8887753@gmail.com>
2026-05-05 18:51:59 +0300

    Split styles into global and components css files

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,8 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import App from "./App";
-import "./App.css";
+import "./global.css";
+import "./components.css";
 import "react-toastify/dist/ReactToastify.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
